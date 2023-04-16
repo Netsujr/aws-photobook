a0e4ff2c856e4a15b69b55828b00ebbe26f3cc82
Renato Delboni <87453991+Netsujr@users.noreply.github.com>
2023-04-16 20:17:32 +0100

    fixed Missing Amplify Brackets

--- src/main.js
+++ src/main.js
@@ -3,5 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './assets/tailwind.css'
+import '@aws-amplify/ui-vue'
+import { Amplify } from "aws-amplify";
+import aws_exports from './aws-exports'
 
-createApp(App).use(store).use(router).mount('#app')
+Amplify.configure(aws_exports);
+
+createApp(App).use(store).use(router).use(Amplify).mount('#app')
