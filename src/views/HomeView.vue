25d5fa58ebc5ce07ff9b7e80c119716a14e6de7c
Renato Delboni <87453991+Netsujr@users.noreply.github.com>
2023-04-23 14:39:06 +0100

    Added Schema + API
    1:23

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,7 @@
           <div class="mb-7">
             <h1 class="text-xl font-bold mb-4">Hello {{ user.username }}!</h1>
             <button
-              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
               @click="signOut"
             >
               Sign Out
